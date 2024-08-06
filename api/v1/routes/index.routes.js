62464292b99812cd03cc35f238c5bd44ca47ad1c
dokhacduong3 <dokhacduong3@gmail.com>
2024-08-06 19:44:51 +0700

    loadhosting4

--- api/v1/routes/index.routes.js
+++ api/v1/routes/index.routes.js
@@ -1,7 +1,7 @@
 const pubgRoutes = require("./pubg.routes");
 const pubgPcRoutes = require("./pubg_pc.routes");
 module.exports = (app) => {
-    const version = "./api/v1";
+    const version = "/api/v1";
     app.use(`${version}/pubg`, pubgRoutes);
     app.use(`${version}/pubg-pc`, pubgPcRoutes);
 };
